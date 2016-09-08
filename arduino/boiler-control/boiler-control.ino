5a68e6585826516150d41a3bbb0616ef129cd190
Mark Gibson <mgibson@adaptavist.com>
2016-09-08 22:53:23 +0100

    Remove ethernet support from arduino sketch

--- arduino/boiler-control/boiler-control.ino
+++ arduino/boiler-control/boiler-control.ino
@@ -1,5 +1,4 @@
 #include <SPI.h>
-#include <Ethernet.h>
 #include <avr/wdt.h>
 
 const unsigned int on[] = {
@@ -70,22 +69,12 @@ const unsigned int off[] = {
 65534,65535
 };
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-
-IPAddress ip(192, 168, 1, 123);
-
-EthernetServer server(80);
-
-String HTTP_line;
-
 #define heatLED 13
 #define txLED 9
 #define netLED 7
 
 #define tx 4
 #define go 6
-#define HTTP false
-#define USB true
 #define DEBUG true
 
 const unsigned long debounce = 3000000;
@@ -111,9 +100,7 @@ unsigned long lastIncoming = 0;
 #define HEART_BEAT 180000
 
 void setup() {
-  if (DEBUG || USB) {
-    Serial.begin(9600);
-  }
+  Serial.begin(9600);
 
   if (DEBUG) {
     Serial.println("started");
@@ -137,17 +124,7 @@ void setup() {
 
   pinMode(go, INPUT_PULLUP);
 
-  if (HTTP) {
-    Ethernet.begin(mac, ip);
-    server.begin();
-
-    if (DEBUG) {
-      Serial.print("server is at ");
-      Serial.println(Ethernet.localIP());
-    }
-  }
-
-  if (USB && DEBUG) {
+  if (DEBUG) {
     Serial.println("usb enabled");
   }
 
@@ -176,87 +153,12 @@ void loop() {
       heat = !heat;
       startTx(true);
     } else {
-      if (HTTP) {
-        http();
-      }
-      if (USB) {
-        usb();
-      }
+      usb();
     }
     
     if (micros() >= lastTransmit+reTransmitPeriod) {
       startTx(false);
     }
-  }
-}
-
-void http() {
-  EthernetClient client = server.available();
-
-  if (client) {
-    startIncoming();
-
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    boolean firstLine = true;
-    boolean newHeat = heat;
-    HTTP_line = "";
-
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-
-        if (firstLine) {
-          HTTP_line += c;
-        }
-
-        if (DEBUG) {
-          Serial.write(c);
-        }
-
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: application/json");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println();
-
-          if (HTTP_line.indexOf("GET /on") == 0) {
-            newHeat = true;
-          } else if (HTTP_line.indexOf("GET /off") == 0) {
-            newHeat = false;
-          } else if (HTTP_line.indexOf("GET /test") == 0) {
-            test_mode = true;
-          } else if (HTTP_line.indexOf("GET /normal") == 0) {
-            test_mode = false;
-          }
-          
-          client.println(newHeat ? "true" : "false");
-
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-          firstLine = false;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-
-    // give the web browser time to receive the data
-    delay(1);
-
-    // close the connection:
-    client.stop();
-
-    endIncoming(newHeat);
   }
 }
 
